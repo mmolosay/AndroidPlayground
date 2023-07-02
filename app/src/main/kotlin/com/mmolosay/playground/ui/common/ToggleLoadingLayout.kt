2344c1121bbfc640cdb9742f409d2092b86ee72f
mmolosay <mmolosays@gmail.com>
2023-07-02 15:30:13 +0200

    Refine KDoc

--- app/src/main/kotlin/com/mmolosay/playground/ui/common/ToggleLoadingLayout.kt
+++ app/src/main/kotlin/com/mmolosay/playground/ui/common/ToggleLoadingLayout.kt
@@ -5,6 +5,9 @@ import androidx.compose.ui.Modifier
 
 /**
  * A variation of [ToggleLayout] which second content is loading component.
+ *
+ * This function is just a convenience for working with a [ToggleLayout].
+ * All it does is just provide better names, like [loading] instead of [ToggleLayout]'s `showFirst`.
  */
 @Composable
 fun ToggleLoadingLayout(
