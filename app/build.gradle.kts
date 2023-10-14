33c23f1213ed8030e66490fe030f3dd11caa5bc7
mmolosay <mmolosays@gmail.com>
2023-10-14 11:52:56 +0200

    Set up debounce test UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.mmolosay.playground"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -53,8 +53,9 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
 
     // Design
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.9.0") // for Material3 xml theme
     implementation("androidx.compose.material3:material3:1.1.1")
+    implementation("androidx.compose.material:material-icons-core:1.4.3")
 
     // Compose
     implementation(platform("androidx.compose:compose-bom:2023.05.01"))
@@ -67,8 +68,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.7.2")
 
     // Third-party
-    implementation("io.github.mmolosay:debounce:1.1.0")
-//    implementation("com.github.mmolosay.resource:resource-context:1.1.3")
+    implementation("io.github.mmolosay:debounce:1.2.0")
 
     // Testing
 }
--- app/src/main/kotlin/com/mmolosay/playground/presentation/ui/MainScreen.kt
+++ app/src/main/kotlin/com/mmolosay/playground/presentation/ui/MainScreen.kt
@@ -1,26 +1,40 @@
 package com.mmolosay.playground.presentation.ui
 
+import android.content.Context
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mmolosay.playground.presentation.design.PlaygroundTheme
 import com.mmolosay.playground.presentation.ui.common.Screen
+import kotlinx.coroutines.launch
+import java.lang.Exception
+import java.time.LocalTime
 
 // region Previews
 
@@ -31,46 +45,91 @@ import com.mmolosay.playground.presentation.ui.common.Screen
 @Composable
 private fun MainPreview() {
     PlaygroundTheme {
-        MainScreen()
+        MainScreen(
+            onButtonClick = { _, _ -> },
+            buttonClickable = true,
+        )
     }
 }
 
 // endregion
 
 @Composable
-fun MainScreen() {
+fun MainScreen(
+    onButtonClick: (String, (String) -> Unit) -> Unit,
+    buttonClickable: Boolean,
+) {
     Screen {
-        Main()
+        Main(
+            onButtonClick = onButtonClick,
+            buttonClickable = buttonClickable,
+        )
     }
 }
 
 @Composable
-fun Main() {
+fun Main(
+    onButtonClick: (String, (String) -> Unit) -> Unit,
+    buttonClickable: Boolean,
+) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(vertical = 8.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        val context = LocalContext.current
-
         var text by remember { mutableStateOf("") }
+        val events = remember { mutableStateListOf<String>() }
+        val eventsListState = rememberLazyListState()
+        val coroutineScope = rememberCoroutineScope()
+
         TextField(
             value = text,
             onValueChange = { text = it },
             modifier = Modifier.fillMaxWidth(0.5f),
         )
 
-        val onClick: () -> Unit = {
-            val reversed = reverseUserInput(text)
-            val message = "$text – $reversed"
-            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        val onEvent: (String) -> Unit = { event ->
+            val now = LocalTime.now().toString()
+            val message = "$now: $event"
+            events.add(message)
+            coroutineScope.launch {
+                eventsListState.scrollToItem(index = events.lastIndex)
+            }
         }
-        Button(onClick = onClick) {
+        val onClick = {
+            onButtonClick(text, onEvent)
+        }
+        val containerColor = MaterialTheme.colorScheme.run {
+            if (buttonClickable) primary else primaryContainer
+        }
+        val buttonColors = ButtonDefaults.buttonColors(
+            containerColor = containerColor,
+        )
+
+        Button(
+            onClick = onClick,
+            colors = buttonColors,
+        ) {
             Text("Reverse input text")
+        }
+
+        LazyColumn(
+            state = eventsListState,
+            reverseLayout = true,
+        ) {
+            items(events) { event ->
+                EventItem(event)
+            }
         }
     }
 }
 
-fun reverseUserInput(text: String) =
-    text.reversed()
+@Composable
+fun EventItem(event: String) {
+    Text(text = event)
+}
+
+fun Context.showToast(text: String) {
+    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+}
