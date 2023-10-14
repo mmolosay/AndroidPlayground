937312d617e737bb73ba0f9d84ff53163bf0b0c0
mmolosay <mmolosays@gmail.com>
2023-10-14 11:55:11 +0200

    Reset UI

--- app/src/main/kotlin/com/mmolosay/playground/presentation/ui/MainActivity.kt
+++ app/src/main/kotlin/com/mmolosay/playground/presentation/ui/MainActivity.kt
@@ -3,22 +3,9 @@ package com.mmolosay.playground.presentation.ui
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.lifecycleScope
 import com.mmolosay.playground.presentation.design.PlaygroundTheme
-import io.github.mmolosay.debounce.debounce
-import io.github.mmolosay.debounce.identity.DebounceStateIdentity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import kotlin.time.Duration.Companion.seconds
 
 class MainActivity : AppCompatActivity() {
-
-    private val buttonState = DebounceStateIdentity()
-    private val buttonClickableFlow = MutableStateFlow(true)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,36 +15,7 @@ class MainActivity : AppCompatActivity() {
     private fun setContent() =
         setContent {
             PlaygroundTheme {
-                MainScreen(
-                    onButtonClick = ::onButtonClick,
-                    buttonClickable = buttonClickableFlow.collectAsStateWithLifecycle().value,
-                )
+                MainScreen()
             }
         }
-
-    private fun onButtonClick(input: String, onEvent: (String) -> Unit) {
-        onEvent("click, attempt")
-        val timeout = 2.seconds
-        val result = buttonState.debounce {
-            buttonClickableFlow.value = false
-            showToast("reversing...")
-            lifecycleScope.launch(Dispatchers.Default) {
-                delay(4.seconds)
-                val reversed = input.reversed()
-                val message = "$input – $reversed"
-                withContext(Dispatchers.Main.immediate) { showToast(message) }
-                onEvent("done: $message")
-
-                releaseIn(timeout)
-                delay(timeout)
-                buttonClickableFlow.value = true
-                onEvent("released")
-            }
-        }
-        if (result) {
-            onEvent("executed")
-        } else {
-            onEvent("debounced")
-        }
-    }
 }
--- app/src/main/kotlin/com/mmolosay/playground/presentation/ui/MainScreen.kt
+++ app/src/main/kotlin/com/mmolosay/playground/presentation/ui/MainScreen.kt
@@ -1,40 +1,35 @@
 package com.mmolosay.playground.presentation.ui
 
-import android.content.Context
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
-import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mmolosay.playground.presentation.design.PlaygroundTheme
 import com.mmolosay.playground.presentation.ui.common.Screen
-import kotlinx.coroutines.launch
-import java.lang.Exception
-import java.time.LocalTime
+
+@Composable
+fun MainScreen() {
+    Screen {
+        Main()
+    }
+}
+
+@Composable
+fun Main() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+
+    }
+}
 
 // region Previews
 
@@ -45,91 +40,8 @@ import java.time.LocalTime
 @Composable
 private fun MainPreview() {
     PlaygroundTheme {
-        MainScreen(
-            onButtonClick = { _, _ -> },
-            buttonClickable = true,
-        )
+        MainScreen()
     }
 }
 
 // endregion
-
-@Composable
-fun MainScreen(
-    onButtonClick: (String, (String) -> Unit) -> Unit,
-    buttonClickable: Boolean,
-) {
-    Screen {
-        Main(
-            onButtonClick = onButtonClick,
-            buttonClickable = buttonClickable,
-        )
-    }
-}
-
-@Composable
-fun Main(
-    onButtonClick: (String, (String) -> Unit) -> Unit,
-    buttonClickable: Boolean,
-) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(vertical = 8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        var text by remember { mutableStateOf("") }
-        val events = remember { mutableStateListOf<String>() }
-        val eventsListState = rememberLazyListState()
-        val coroutineScope = rememberCoroutineScope()
-
-        TextField(
-            value = text,
-            onValueChange = { text = it },
-            modifier = Modifier.fillMaxWidth(0.5f),
-        )
-
-        val onEvent: (String) -> Unit = { event ->
-            val now = LocalTime.now().toString()
-            val message = "$now: $event"
-            events.add(message)
-            coroutineScope.launch {
-                eventsListState.scrollToItem(index = events.lastIndex)
-            }
-        }
-        val onClick = {
-            onButtonClick(text, onEvent)
-        }
-        val containerColor = MaterialTheme.colorScheme.run {
-            if (buttonClickable) primary else primaryContainer
-        }
-        val buttonColors = ButtonDefaults.buttonColors(
-            containerColor = containerColor,
-        )
-
-        Button(
-            onClick = onClick,
-            colors = buttonColors,
-        ) {
-            Text("Reverse input text")
-        }
-
-        LazyColumn(
-            state = eventsListState,
-            reverseLayout = true,
-        ) {
-            items(events) { event ->
-                EventItem(event)
-            }
-        }
-    }
-}
-
-@Composable
-fun EventItem(event: String) {
-    Text(text = event)
-}
-
-fun Context.showToast(text: String) {
-    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
-}
