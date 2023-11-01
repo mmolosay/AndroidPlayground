b8980e2c1141ac47198382de26f1395bd6506f8e
mmolosay <mmolosays@gmail.com>
2023-11-01 22:04:16 +0100

    Raise Compose BOM version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    compileSdk = 33
+    compileSdk = 34
     namespace = "com.mmolosay.playground"
 
     defaultConfig {
@@ -58,7 +58,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-core:1.4.3")
 
     // Compose
-    implementation(platform("androidx.compose:compose-bom:2023.05.01"))
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     // https://developer.android.com/jetpack/compose/bom/bom-mapping
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.foundation:foundation")
