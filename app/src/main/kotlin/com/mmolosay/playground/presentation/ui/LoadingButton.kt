aea2b67b4e1b2703578fabfa727a0bb9c33906ee
mmolosay <mmolosays@gmail.com>
2023-11-01 19:42:30 +0100

    Refactor

--- app/src/main/kotlin/com/mmolosay/playground/presentation/ui/LoadingButton.kt
+++ app/src/main/kotlin/com/mmolosay/playground/presentation/ui/LoadingButton.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.Transition
+import androidx.compose.animation.core.VisibilityThreshold
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.updateTransition
@@ -35,6 +36,7 @@ import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import com.mmolosay.playground.presentation.design.PlaygroundTheme
 
@@ -91,11 +93,11 @@ private fun PrimaryContent(
     loadingStateTransition: Transition<Boolean>,
 ) {
     val density = LocalDensity.current
-    val softEdgeWidthPx = remember(density) { with(density) { 15.dp.toPx() } }
+    val softEdgeWidthPx = remember(density) { with(density) { 20.dp.toPx() } }
     val softEdgeProgress by loadingStateTransition.animateFloat(
         transitionSpec = {
             spring(
-                stiffness = Spring.StiffnessMediumLow,
+                stiffness = SpringStiffness,
             )
         },
         targetValueByState = { loading -> if (loading) 1f else 0f },
@@ -103,63 +105,32 @@ private fun PrimaryContent(
     )
     loadingStateTransition.AnimatedVisibility(
         visible = { loading -> !loading },
-        enter = fadeIn() + expandHorizontally(expandFrom = Alignment.CenterHorizontally),
+        enter = fadeIn() + expandHorizontally(
+            animationSpec = spring(
+                stiffness = SpringStiffness,
+                dampingRatio = WidthAnimationSpringDumpingRatio,
+                visibilityThreshold = IntSize.VisibilityThreshold,
+            ),
+            expandFrom = Alignment.CenterHorizontally,
+        ),
         exit = fadeOut(
             animationSpec = spring(
                 stiffness = SpringStiffness,
                 visibilityThreshold = FadeOutVisibilityThreshold,
             ),
         ) + shrinkHorizontally(
+            animationSpec = spring(
+                stiffness = SpringStiffness,
+                // dampingRatio is not applicable here, size cannot become negative
+                visibilityThreshold = IntSize.VisibilityThreshold,
+            ),
             shrinkTowards = Alignment.CenterHorizontally,
         ),
         modifier = Modifier
-            .graphicsLayer(compositingStrategy = CompositingStrategy.Offscreen)
-            .drawWithCache {
-                @Suppress("NAME_SHADOWING")
-                val softEdgeProgress = softEdgeProgress
-                val gradientColorsLtr = listOf(Color.Transparent, Color.Black)
-                val gradientColorsRtl = gradientColorsLtr.reversed()
-                fun directionalHorizontalGradient(ltr: Boolean) =
-                    Brush.horizontalGradient(
-                        colors = if (ltr) gradientColorsLtr else gradientColorsRtl,
-                        endX = softEdgeWidthPx,
-                    )
-
-                val leftSoftEdgeBrush = directionalHorizontalGradient(ltr = true)
-                val rightSoftEdgeBrush = directionalHorizontalGradient(ltr = false)
-                val softEdgeSize = Size(width = softEdgeWidthPx, height = size.height)
-                val blendMode = BlendMode.DstIn
-                onDrawWithContent {
-                    drawContent()
-                    if (size.width == 0f) return@onDrawWithContent
-                    val x = if (softEdgeProgress <= 0.30f) {
-                        val sectionProgress = softEdgeProgress / 0.30f
-                        -softEdgeWidthPx + (softEdgeWidthPx * sectionProgress)
-                    } else {
-                        val halfWidth = size.width / 2
-                        if (halfWidth >= softEdgeWidthPx) {
-                            0f
-                        } else {
-                            halfWidth - softEdgeWidthPx
-                        }
-                    }
-                    translate(left = x) {
-                        drawRect(
-                            brush = leftSoftEdgeBrush,
-                            size = softEdgeSize,
-                            blendMode = blendMode,
-                        )
-                    }
-                    translate(left = size.width - softEdgeWidthPx) {
-                        drawRect(
-                            brush = rightSoftEdgeBrush,
-                            topLeft = Offset.Zero.copy(x = -x),
-                            size = softEdgeSize,
-                            blendMode = blendMode,
-                        )
-                    }
-                }
-            },
+            .horizontalSoftEdge(
+                progress = { softEdgeProgress },
+                edgeWidthPx = softEdgeWidthPx,
+            ),
     ) {
         Text(
             text = "Place the order",
@@ -167,8 +138,64 @@ private fun PrimaryContent(
     }
 }
 
+private fun Modifier.horizontalSoftEdge(
+    progress: () -> Float,
+    edgeWidthPx: Number,
+): Modifier =
+    this
+        .graphicsLayer(compositingStrategy = CompositingStrategy.Offscreen)
+        .drawWithCache {
+            val softEdgeProgress = progress()
+            val edgeWidth = edgeWidthPx.toFloat()
+
+            val gradientColorsLtr = listOf(Color.Transparent, Color.Black)
+            val gradientColorsRtl = gradientColorsLtr.reversed()
+            fun directionalHorizontalGradient(ltr: Boolean) =
+                Brush.horizontalGradient(
+                    colors = if (ltr) gradientColorsLtr else gradientColorsRtl,
+                    endX = edgeWidth,
+                )
+
+            val leftSoftEdgeBrush = directionalHorizontalGradient(ltr = true)
+            val rightSoftEdgeBrush = directionalHorizontalGradient(ltr = false)
+            val softEdgeSize = Size(width = edgeWidth, height = size.height)
+            val blendMode = BlendMode.DstIn
+
+            onDrawWithContent {
+                drawContent()
+                if (size.width == 0f) return@onDrawWithContent
+                val x = if (softEdgeProgress <= 0.30f) {
+                    val sectionProgress = softEdgeProgress / 0.30f
+                    -edgeWidth + (edgeWidth * sectionProgress)
+                } else {
+                    val halfWidth = size.width / 2
+                    if (halfWidth >= edgeWidth) {
+                        0f
+                    } else {
+                        halfWidth - edgeWidth
+                    }
+                }
+                translate(left = x) {
+                    drawRect(
+                        brush = leftSoftEdgeBrush,
+                        size = softEdgeSize,
+                        blendMode = blendMode,
+                    )
+                }
+                translate(left = size.width - edgeWidth) {
+                    drawRect(
+                        brush = rightSoftEdgeBrush,
+                        topLeft = Offset.Zero.copy(x = -x),
+                        size = softEdgeSize,
+                        blendMode = blendMode,
+                    )
+                }
+            }
+        }
+
 private val SpringStiffness = Spring.StiffnessMediumLow
 private val FadeOutVisibilityThreshold = 0.10f
+private val WidthAnimationSpringDumpingRatio = Spring.DampingRatioLowBouncy
 
 @Preview
 @Composable
