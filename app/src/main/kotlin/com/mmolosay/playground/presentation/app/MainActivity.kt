32743071a2c7f939d899933f8c63cffaed0a2292
mmolosay <mmolosays@gmail.com>
2025-02-01 16:05:41 +0100

    Remove window background
    When system Dark mode is OFF, but user-selected theme is "Force Dark", then app appears in dark theme. However, when changing screens, you can see a short white blink. It happens because cross-fade animation is applied to both source and destination screens. While the animation is running, and both screens are translucent, you can see white window background underneath. It is set by XML theme of the 'MainActivity', which adheres to Dark mode being turned OFF, meaning the app's theme is light
    
    Commit message is copied from https://github.com/mmolosay/TheColor, commit d973faf9f9eb6a784e5b037c8547ee11626f8925

--- app/src/main/kotlin/com/mmolosay/playground/presentation/app/MainActivity.kt
+++ app/src/main/kotlin/com/mmolosay/playground/presentation/app/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        window.setBackgroundDrawable(null)
         setContent()
     }
 
